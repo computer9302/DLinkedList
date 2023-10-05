327e2d905eb5e87554a8ffec302d3be6d53b90f1
computer9302 <computer9302@gmail.com>
2023-10-05 22:02:01 +0900

    DoublyLinkedList

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,13 @@
+package org.example;
+
+class Node<E> {
+    E data;
+    Node<E> next;
+    Node<E> prev;
+
+    Node(E data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
